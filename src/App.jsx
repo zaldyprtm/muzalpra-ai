2b93a87e3e028823c96c6c36e4b5ee5c97da7ee0
zaldyprtm <zaldyprtm@gmail.com>
2024-05-29 13:14:57 +0700

    add file

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./App.css";
+import { requestToGroqAI } from "./utils/groq";
+import { Light as SyntexHighlight } from "react-syntax-highlighter";
+import { darcula } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+function App() {
+  const [data, setData] = useState("");
+
+  const handleSubmit = async () => {
+    const ai = await requestToGroqAI(content.value);
+    setData(ai);
+  };
+  return (
+    <>
+      <main className="flex flex-col min-h-[80vh] justify-center items-center max-w-xl w-full mx-auto rounded-md">
+        <h1 className="text-4xl font-bold text-indigo-500">REACT|GROQ</h1>
+
+        {/* form */}
+        <form className="flex flex-col gap-4 py-4 w-full">
+          <input
+            type="text"
+            placeholder="ketik permintaan disini..."
+            id="content"
+            name="content"
+            className="py-2 px-4 text-md rounded-md"
+          />
+          <button
+            type="button"
+            onClick={handleSubmit}
+            className="bg-sky-500 text-white py-2 px-4 font-semibold  rounded-lg mx-auto"
+          >
+            Submit
+          </button>
+        </form>
+        {/* kotak untuk menampilkan hasil pencarian atau permintaan user */}
+        <div className="max-w-xl rounded-lg w-full mx-auto">
+          {data ? (
+
+            <SyntexHighlight language="swift" style={darcula} wrapLongLines={true}>
+            {data}
+          </SyntexHighlight>
+          ) : null }
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/utils/groq.js
@@ -0,0 +1,18 @@
+import {Groq} from "groq-sdk"
+
+const GROQ_API = import.meta.env.VITE_GROQ;
+const groq = new Groq({
+  dangerouslyAllowBrowser:true,
+  apiKey: GROQ_API,
+})
+
+export const requestToGroqAI = async (content) => {
+    const reply = await groq.chat.completions.create({
+        messages: [{
+            role: "user",
+            content,
+        }],
+        model: "llama3-8b-8192",
+    })
+    return reply.choices[0].message.content;
+}
