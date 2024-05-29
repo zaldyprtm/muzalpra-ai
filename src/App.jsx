7ee4769549a393c6fc91f184bd3f34a76a81bbdf
zaldyprtm <zaldyprtm@gmail.com>
2024-05-29 15:52:41 +0700

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { requestToGroqAI } from "./utils/groq";
 import { Light as SyntexHighlight } from "react-syntax-highlighter";
 import { darcula } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import Navbar from "./layouts/Navbar";
 
 function App() {
   const [data, setData] = useState("");
@@ -13,6 +14,7 @@ function App() {
   };
   return (
     <>
+    <Navbar />
       <main className="flex flex-col min-h-[80vh] justify-center items-center max-w-xl w-full mx-auto rounded-md">
         <h1 className="text-4xl font-bold text-indigo-500">MUZALPRA|AI</h1>
 
@@ -23,7 +25,7 @@ function App() {
             placeholder="ketik permintaan disini..."
             id="content"
             name="content"
-            className="py-2 px-4 text-md rounded-md"
+            className="py-2 px-4 text-md rounded-md w-56 mx-auto"
           />
           <button
             type="button"
---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar bg-base-300">
+        <button className="btn btn-ghost text-xl">daisyUI</button>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
