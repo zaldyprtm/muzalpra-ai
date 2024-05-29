eaef210bd7b46b04c2f7d7a215e0763b2718bd9f
zaldyprtm <zaldyprtm@gmail.com>
2024-05-29 15:53:53 +0700

    navbar

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <>
       <div className="navbar bg-base-300">
-        <button className="btn btn-ghost text-xl">daisyUI</button>
+        <button className="btn btn-ghost text-xl">Muzalpra AI</button>
       </div>
     </>
   );
